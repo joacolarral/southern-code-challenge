82d0934c24840aedd5eb7c4024e2c9b90ddaa690
Joaquin Larralde <joaquinlarralde@Joaquins-MacBook-Pro.local>
2022-01-23 20:44:49 -0300

    feat: component footer added

---
+++ components/Footer/index.js
@@ -0,0 +1,9 @@
+import { Layout } from 'antd';
+
+const { Footer } = Layout;
+
+export default function FooterComponent() {
+  return (
+    <Footer style={{ textAlign: 'center' }}>Designed by Joaquin Larrade</Footer>
+  );
+}
